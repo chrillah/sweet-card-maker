eb6f962b27fb1eeeb4d2f666381f9a2ceb31bfce
chrillah <ch.ronnberg@gmail.com>
2023-02-06 19:06:55 +0100

    add css-style-sheet

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
+import '../assets/main.css'
+
 createApp(App).use(router).use(store).use(bootstrap).mount('#app')
