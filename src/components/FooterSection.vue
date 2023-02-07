d016d00a06ec575e2d3b895f833903c303ea8148
chrillah <ch.ronnberg@gmail.com>
2023-02-07 16:51:33 +0100

    footer added

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="footer-wrapper">
+    <div class="footer-container">
+      <div class="footer-information">
+        <h1>Footer</h1>
+      </div>
+    </div>
+  </div>
+</template>
