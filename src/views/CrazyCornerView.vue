0c70df107653720e72f43f758b0f8193c1dc7b54
chrillah <ch.ronnberg@gmail.com>
2023-02-08 13:57:19 +0100

    crazy corner updated

--- src/views/CrazyCornerView.vue
+++ src/views/CrazyCornerView.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="wrapper">
     <div class="container">
-      <ChildComponent />
+      <ChildComponent @input-value-from-child="fromChild" />
+      <h1 class="bg">{{ msg }}</h1>
+      <CounterButton class="button-28" />
     </div>
   </div>
 </template>
 
 <script>
   import ChildComponent from '../components/ChildComponent.vue'
+  import CounterButton from '../components/CounterButton.vue'
   export default {
     components: {
-      ChildComponent
+      ChildComponent,
+      CounterButton
+    },
+    data() {
+      return {
+        msg: ''
+      }
+    },
+    methods: {
+      fromChild(input) {
+        this.msg = input
+      }
     }
   }
 </script>
+<style>
+.bg{
+  font-family: monospace;
+  text-align: center;
+  padding: 1rem;
+  width: 100%;
+  background-color: #ffffff;
+}
+</style>
