de075149adec8023b04cbab1f792a7e0811a0bca
chrillah <ch.ronnberg@gmail.com>
2023-02-11 20:17:12 +0100

    userinput updated

--- src/components/UserInput.vue
+++ src/components/UserInput.vue
@@ -3,8 +3,6 @@
     <div class="input-container">
       <p class="label">Write a quote</p>
       <input type="text" v-model="quote" />
-      <!-- <p v-if="quote">Here is your quote</p>
-      <h1>{{ quote }}</h1> -->
     </div>
     <input
       v-if="quote"
@@ -33,9 +31,6 @@
       quote(newQuote, oldQuote) {
         console.log(`${oldQuote} has changed to ${newQuote}`)
       }
-    },
-    computed(){
-      return this.quote
     }
   }
 </script>
