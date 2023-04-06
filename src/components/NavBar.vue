7a68e8e7c8ed798925fb8ee84c686cdf93766354
chrillah <ch.ronnberg@gmail.com>
2023-04-06 17:26:14 +0200

    products removed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <RouterLink class="navbar-brand" to="/"><img class="logo" src="/assets/img/logo_v3.png" alt="" /></RouterLink>
+      <RouterLink class="navbar-brand" to="/"
+        ><img class="logo" src="/assets/img/logo_v3.png" alt=""
+      /></RouterLink>
       <button
         class="navbar-toggler"
         type="button"
@@ -15,11 +17,14 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <RouterLink class="nav-link" aria-current="page" to="/">Home</RouterLink>
+          <RouterLink class="nav-link" aria-current="page" to="/"
+            >Home</RouterLink
+          >
           <RouterLink class="nav-link" to="/collection">Collection</RouterLink>
           <RouterLink class="nav-link" to="/diy">DIY</RouterLink>
-          <RouterLink class="nav-link" to="/poster">{{ numberOfCards }}</RouterLink>
-          <RouterLink class="nav-link" to="/products">Products</RouterLink>
+          <RouterLink class="nav-link" to="/poster">{{
+            numberOfCards
+          }}</RouterLink>
         </div>
       </div>
     </div>
@@ -28,41 +33,43 @@
   <FooterSection />
 </template>
 <script>
-import FooterSection from './FooterSection.vue';
-export default{
-  components:{
-    FooterSection
-  },
-  computed:{
-    numberOfCards(){
+  import FooterSection from './FooterSection.vue'
+  export default {
+    components: {
+      FooterSection
+    },
+    computed: {
+      numberOfCards() {
         return 'Your Cards ' + this.$store.state.cards.length
+      }
     }
   }
-}
 </script>
 <style scoped>
-.navbar{
-  z-index: 100;
-  width: 100%;
-  position: fixed;
-  background-color: #FFFFFF;
-}
-.logo{
-  width: 50px;
-  height: auto;
-}
+  .navbar {
+    z-index: 100;
+    width: 100%;
+    position: fixed;
+    background-color: #ffffff;
+  }
+  .logo {
+    width: 50px;
+    height: auto;
+  }
 
-.nav-link{
-  font-family: monospace;
-  font-weight: 800;
-  color: black;
-}
-.nav-link:hover,.nav-link:focus, .logo:hover{
-  background-color: var(--active-color);
-  font-weight: 400;
-}
+  .nav-link {
+    font-family: monospace;
+    font-weight: 800;
+    color: black;
+  }
+  .nav-link:hover,
+  .nav-link:focus,
+  .logo:hover {
+    background-color: var(--active-color);
+    font-weight: 400;
+  }
 
-.navbar-toggler{
-  color: #000000;
-}
+  .navbar-toggler {
+    color: #000000;
+  }
 </style>
--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ import HomeView from './views/HomeView.vue'
 import DiyView from './views/DiyView.vue'
 import UserCardsView from './views/UserCardsView.vue'
 import SavedPosterView from './views/SavedPosterView.vue'
-import ProductsView from './views/ProductsView.vue'
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -23,18 +22,14 @@ export default createRouter({
       path: '/'
     },
     {
-      component : UserCardsView,
-      name : 'user',
+      component: UserCardsView,
+      name: 'user',
       path: '/user/:inputFromUser'
     },
     {
-      component : SavedPosterView,
+      component: SavedPosterView,
       name: 'poster',
-      path : '/poster'
-    },
-    {
-      component : ProductsView,
-      path :  "/products"
+      path: '/poster'
     }
   ]
 })
